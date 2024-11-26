269783e7c6e0dc04a1772b36b96fb2679b1357f6
ArthurzinLindo <juvenalfp14@gmail.com>
2024-11-26 20:44:43 -0300

    pequenas alterações

--- tcc-2024-projeto-web/src/Pag/PagHome/Home.js
+++ tcc-2024-projeto-web/src/Pag/PagHome/Home.js
@@ -9,6 +9,8 @@ function Home() {
         <header className= "Cabeçalho"></header>
         <div className="btnlogin"><button type="button" class="btn btn-outline-secondary">Login</button></div>
         <div className="btncadastro"></div>
+        <h1 id="logado"></h1> 
+        <button onclick="sair()">Sair</button>
       </div>
     </div>
 
